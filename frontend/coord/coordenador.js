39b60552c51b945b7a16e1315b0eea75b3997f57
LucasEmmanoel06 <lucas.albuquerque2k@gmail.com>
2024-10-07 10:34:42 -0300

    Espaçamento na linha de caixas de baixo

--- frontend/coord/coordenador.js
+++ frontend/coord/coordenador.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
     const nome = localStorage.getItem('nome');
     $('#nomeCoordenador').text(nome);
   
+    // Função de logout
+    document.getElementById('logoutButton').addEventListener('click', function() {
+      // Remover o JWT do armazenamento local
+      localStorage.removeItem('jwtToken');
+      // Redirecionar para a página de login
+      window.location.href = '../login.html';
+    });
+    
     // Função para buscar e exibir as turmas
     function loadTurmas() {
       $.ajax({
@@ -18,11 +26,11 @@ $(document).ready(function() {
           } else {
             response.forEach(turma => {
               const turmaCard = `
-                <div class="col-md-4">
+                <div class="col-md-4 mb-4">
                   <div class="card turma-card">
                     <div class="card-body">
                       <h5 class="card-title">${turma.nome}</h5>
-                      <p class="card-text">Descrição: ${turma.descricao}</p>
+                      <p class="card-text">Ano: ${turma.ano}</p>
                       <a href="#" class="btn btn-primary">Ver detalhes</a>
                     </div>
                   </div>
