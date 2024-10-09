07631f2c24dd068402837ca754cdd47064eb3ee0
Lucas Emmanoel <125286130+LucasEmmanoel06@users.noreply.github.com>
2024-10-09 11:24:17 -0300

    Delete backend directory

--- backend/models/disciplinas.models.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DisciplinasSchema = new mongoose.Schema({
-    nome: { type: String, required: true },
-    professores: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Usuarios' }],
-    turmas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Turma',}],
-});
-
-module.exports = mongoose.model('Disciplinas', DisciplinasSchema);
--- backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registrar, login } = require('../controllers/auth.controller');
-const authMiddleware = require('../middleware/auth.middleware');
-const router = express.Router();
-
-router.post('/registrar', authMiddleware, registrar);
-router.post('/login', login);
-
-module.exports = router;
